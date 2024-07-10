8499e93cc42ffa6a7f02bda3872184062c730a45
David Suárez <dsuarez@comparaonline.com>
2024-07-10 10:45:11 -0600

    fix: update main file

--- src/main.py
+++ src/main.py
@@ -1,4 +1,9 @@
-import github_client.deploy_status as github
+# import github_client.deploy_status as github
+
+# if __name__ == "__main__":
+#     github.deploy_status()
+
+from github_client import deploy_status as gh
 
 if __name__ == "__main__":
-    github.deploy_status()
+    gh.deploy_status()
